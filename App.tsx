250986ca69404b7090cc5a3f4e9724f636253ecb
sumandharavath <sumandharavath098@gmail.com>
2024-10-28 17:41:58 +0530

    commited changes

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Text>hello suman!</Text>
+    </View>
+  );
+};
+
+export default App;
